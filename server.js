dd7f2ad4e1fd2aecfbaa2c0acec0d7da9215ffac
Laxman Chowdary Kakara <laxmanjs2002@gmail.com>
2026-05-03 11:55:40 -0500

    Initial commit: Workforce Planner Backend

---
+++ server.js
@@ -0,0 +1,2 @@
+import app from "./app.js"; import { env } from "./config/env.js"; import { connectDB } from "./config/db.js"; async function startServer() { await connectDB(); app.listen(env.port, () => { console.log(`Server running on http://localhost:${env.port}`); });
+} startServer();
